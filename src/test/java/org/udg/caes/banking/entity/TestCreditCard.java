06e56d3bca0755f2b1075d689d91431032954ee4
u1989938 <u1989938@campus.udg.edu>
2026-09-21 19:06:08 +0200

    finished initial test

--- src/test/java/org/udg/caes/banking/entity/TestCreditCard.java
+++ src/test/java/org/udg/caes/banking/entity/TestCreditCard.java
@@ -1,10 +1,19 @@
 package org.udg.caes.banking.entity;
 
 import org.junit.jupiter.api.Test;
+import org.udg.caes.banking.exceptions.ClientNotFound;
+import org.udg.caes.banking.service.ClientService;
+import org.udg.caes.banking.service.CreditCardService;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class TestCreditCard {
     @Test
-    void testCredit() {
-        
+    void testCredit() throws ClientNotFound {
+
+        CreditCard cc = new CreditCard("test");
+        cc.credit(100);
+        assertEquals(100, cc.getCredit());
     }
 }
